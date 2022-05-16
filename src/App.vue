ec817f43ac5e0cfa7e3a9748e62cb7dacab049e9
Pajehome <abdel20069@gmail.com>
2022-05-16 13:39:52 +0200

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+  fdf
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style  lang="scss">
+@import "./assets/styles/general.scss";
+#app{
+  color: $butterCup;
+}
+</style>
